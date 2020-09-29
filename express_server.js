2130bdfb81ad28b3cbefeffa5c9b91b25e34a771
Chantal Snazel <clsnazel@gmail.com>
2020-09-29 04:15:54 +0000

    added new url pages with form submission POST functionality

--- express_server.js
+++ express_server.js
@@ -7,18 +7,59 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+
+const generateRandomString = function() {
+  let newLength = 6;
+  let newString = "";
+  //coinflip number or char
+  for(let i = 0; i < newLength; i++ ) {
+    let charNumCoin = Math.random();
+    //if number pick random num between 0-9
+    if (charNumCoin > .5) {
+      let randomNum = Math.floor(Math.random() * 10);
+      newString += randomNum.toString();
+    } else {
+      //else char coin flip upper/lower case
+      let caseCoin = Math.random();
+      //generate random unicode index offset from 0-26
+      let charIndex = Math.floor(Math.random() * 27);
+      //first letter in lowercase latin is 97
+      let firstLetterCode = 97;
+      if (caseCoin > .5) {
+        //first letter in uppercase latin is 65
+        firstLetterCode = 65;
+      } 
+      //add uppercase. Using string from char code with index as offset
+      newString += String.fromCharCode(firstLetterCode + charIndex);
+
+    }
+  }
+  return newString;
+}
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
-app.get('/urls.json', (req, res) => {
+app.get('/urls', (req, res) => {
   //res.json(urlDatabase);
   const templateVars = {title:"URL Index", urls: urlDatabase};
   res.render('pages/urls_index', templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('pages/urls_new');
+});
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  let newCode = generateRandomString();
+  urlDatabase[newCode] = req.body.longURL;
+  res.send('OK');
+});
 app.get('/urls/:shortURL', (req, res) => {
 
   // res.end(`${req.params}`);
